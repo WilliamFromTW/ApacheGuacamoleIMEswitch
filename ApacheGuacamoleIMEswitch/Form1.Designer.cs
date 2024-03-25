d903990d7cdc537de792bf34c24028b46ae79e09
william <william.fromtw@gmail.com>
2024-03-25 18:53:09 +0800

    改成常駐型態

--- ApacheGuacamoleIMEswitch/Form1.Designer.cs
+++ ApacheGuacamoleIMEswitch/Form1.Designer.cs
@@ -30,29 +30,18 @@ namespace ApacheGuacamoleIMEswitch
         /// </summary>
         private void InitializeComponent()
         {
-            buttonGuacamole = new Button();
+            components = new Container();
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
-            button2 = new Button();
             label1 = new Label();
             save = new Button();
             URL = new TextBox();
             textBox1 = new TextBox();
+            notifyIcon1 = new NotifyIcon(components);
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             SuspendLayout();
-            // 
-            // buttonGuacamole
-            // 
-            buttonGuacamole.Font = new Font("Verdana", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonGuacamole.ForeColor = SystemColors.HotTrack;
-            buttonGuacamole.Location = new Point(16, 113);
-            buttonGuacamole.Name = "buttonGuacamole";
-            buttonGuacamole.Size = new Size(713, 63);
-            buttonGuacamole.TabIndex = 0;
-            buttonGuacamole.Text = "按此「開啟酪梨醬(遠端連線)」";
-            buttonGuacamole.UseVisualStyleBackColor = true;
-            buttonGuacamole.Click += button1_Click;
             // 
             // tabControl1
             // 
@@ -60,44 +49,30 @@ namespace ApacheGuacamoleIMEswitch
             tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(758, 451);
+            tabControl1.Size = new Size(532, 245);
             tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            tabPage1.Controls.Add(button2);
             tabPage1.Controls.Add(label1);
             tabPage1.Controls.Add(save);
             tabPage1.Controls.Add(URL);
             tabPage1.Controls.Add(textBox1);
-            tabPage1.Controls.Add(buttonGuacamole);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(750, 423);
+            tabPage1.Size = new Size(524, 217);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Main";
             tabPage1.UseVisualStyleBackColor = true;
             tabPage1.Click += tabPage1_Click;
-            // 
-            // button2
-            // 
-            button2.Font = new Font("Verdana", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.ForeColor = Color.Crimson;
-            button2.Location = new Point(16, 25);
-            button2.Name = "button2";
-            button2.Size = new Size(713, 61);
-            button2.TabIndex = 6;
-            button2.Text = "按此「停用本機左邊Shift鍵」";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click_1;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.HotTrack;
-            label1.Location = new Point(16, 204);
+            label1.Location = new Point(16, 22);
             label1.Name = "label1";
             label1.Size = new Size(173, 18);
             label1.TabIndex = 5;
@@ -108,7 +83,7 @@ namespace ApacheGuacamoleIMEswitch
             // save
             // 
             save.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            save.Location = new Point(668, 222);
+            save.Location = new Point(444, 53);
             save.Name = "save";
             save.Size = new Size(61, 31);
             save.TabIndex = 4;
@@ -120,11 +95,10 @@ namespace ApacheGuacamoleIMEswitch
             // 
             URL.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
             URL.ImeMode = ImeMode.Disable;
-            URL.Location = new Point(16, 225);
+            URL.Location = new Point(16, 53);
             URL.Name = "URL";
-            URL.Size = new Size(646, 27);
+            URL.Size = new Size(407, 27);
             URL.TabIndex = 2;
-            URL.TextAlign = HorizontalAlignment.Center;
             URL.TextChanged += URL_TextChanged;
             URL.KeyPress += URL_KeyPress;
             // 
@@ -133,26 +107,39 @@ namespace ApacheGuacamoleIMEswitch
             textBox1.BackColor = Color.FromArgb(224, 224, 224);
             textBox1.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             textBox1.ForeColor = Color.Blue;
-            textBox1.Location = new Point(16, 258);
+            textBox1.Location = new Point(16, 104);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(713, 117);
+            textBox1.Size = new Size(489, 97);
             textBox1.TabIndex = 1;
-            textBox1.Text = "\r\n使用方式：\r\n1. 「停用本機左邊Shift鍵」暫時停止本機切換中文功能，以免與遠端主機中文切換相衝突\r\n2. 「開啟酪梨醬(遠端連線)」則帶出瀏覽器，並連到預設的遠端連線網址\r\n3. 「啟用本機左邊Shift鍵」則恢復本機切換中文功能\r\n\r\n";
+            textBox1.Text = "\r\n本程式縮小後，會在右下角常駐，按滑鼠右鍵會出現以下功能\r\n「暫停本機左邊Shift鍵切換中文功能」：以免與遠端中文切換衝突\r\n「恢復本機左邊Shift鍵切換中文功能」：恢復本機切換中文功能\r\n「開啟酪梨醬(遠端連線)」：帶出瀏覽器，並連到預設的遠端連線網址\r\n\r\n";
             textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.BalloonTipText = "ApacheGuacamole";
+            notifyIcon1.BalloonTipTitle = "ApacheGuacamole";
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "ApacheGuacamole";
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseClick += notifyIcon1_MouseClick;
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 478);
+            ClientSize = new Size(556, 271);
             Controls.Add(tabControl1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             ImeMode = ImeMode.Off;
+            MaximizeBox = false;
             Name = "Form1";
-            Text = "解決酪梨醬中文切換問題  2.1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "解決酪梨醬中文切換問題  3.0";
             FormClosing += Form1_Closing;
             Load += Form1_Load;
+            Resize += Form1_Resize;
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
@@ -160,14 +147,12 @@ namespace ApacheGuacamoleIMEswitch
         }
 
         #endregion
-
-        private Button buttonGuacamole;
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TextBox textBox1;
         private TextBox URL;
         private Button save;
         private Label label1;
-        private Button button2;
+        private NotifyIcon notifyIcon1;
     }
 }
--- ApacheGuacamoleIMEswitch/Form1.cs
+++ ApacheGuacamoleIMEswitch/Form1.cs
@@ -1,8 +1,11 @@
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Windows.Forms;
 using System.Xml.Linq;
 
 namespace ApacheGuacamoleIMEswitch
@@ -12,6 +15,7 @@ namespace ApacheGuacamoleIMEswitch
         Boolean bIsDisable = false;
         String strURL = "https://guacamole.apache.org/";
 
+        private static ContextMenuStrip contextMenuStrip;
         public Form1()
         {
             InitializeComponent();
@@ -24,8 +28,6 @@ namespace ApacheGuacamoleIMEswitch
                 ps.AddCommand("Set-ItemProperty").AddParameter("Path", "HKCU:\\Software\\Microsoft\\IME\\15.0\\IMETC").AddParameter("Name", "Left Shift Usage").AddParameter("Value", "0x00000002");
                 var results = ps.Invoke<string>();
                 bIsDisable = true;
-                button2.Text = "按此「啟用本機左邊Shift鍵」";
-                button2.ForeColor = Color.Blue;
             }
             /*
             using (var ps = PowerShell.Create())
@@ -46,8 +48,33 @@ namespace ApacheGuacamoleIMEswitch
 
         }
 
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized
+            //hide it from the task bar
+            //and show the system tray icon (represented by the NotifyIcon control)
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+
 
             if (Properties.Settings.Default.URL is null || Properties.Settings.Default.URL.ToString().Trim().Equals(""))
             {
@@ -55,10 +82,68 @@ namespace ApacheGuacamoleIMEswitch
                 Properties.Settings.Default.Save();
                 URL.Text = strURL;
             }
-            else URL.Text = Properties.Settings.Default.URL.ToString();
+            else
+            {
+                URL.Text = Properties.Settings.Default.URL.ToString();
+                strURL = URL.Text.ToString();
+            }
 
-            strURL = URL.Text.ToString();
+            contextMenuStrip = new ContextMenuStrip();
+            ToolStripItem stripItem0 = new ToolStripMenuItem("按此「暫停本機左邊Shift鍵」");
+            stripItem0.ForeColor = Color.Red;
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { stripItem0 });
+            stripItem0.Click += new EventHandler(MenuItem0);
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { stripItem0 });
+
+            ToolStripItem stripItem1 = new ToolStripMenuItem("按此「開啟酪梨醬(遠端連線)」");
+            stripItem1.Click += new EventHandler(MenuItem1);
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { stripItem1 });
+
+            ToolStripItem line = new ToolStripSeparator();
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { line });
+
+            ToolStripItem stripItem2 = new ToolStripMenuItem("離開本程式");
+            stripItem2.Click += new EventHandler(MenuItem3);
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { stripItem2 });
+
+            notifyIcon1.ContextMenuStrip = contextMenuStrip;
+            restoreLeftShift();
+
         }
+
+        private void MenuItem0(object Sender, EventArgs e)
+        {
+            switchNotifyLeftShift();
+
+        }
+
+
+        private void MenuItem1(object Sender, EventArgs e)
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.AddCommand("Set-ItemProperty").AddParameter("Path", "HKCU:\\Software\\Microsoft\\IME\\15.0\\IMETC").AddParameter("Name", "Left Shift Usage").AddParameter("Value", "0x00000002");
+                var results = ps.Invoke<string>();
+                bIsDisable = true;
+                notifyIcon1.ContextMenuStrip.Items[0].Text = "按此「恢復本機左邊Shift鍵」";
+                notifyIcon1.ContextMenuStrip.Items[0].ForeColor = Color.Blue;
+                notifyIcon1.ContextMenuStrip.Update();
+
+            }
+            Process.Start(new ProcessStartInfo(strURL) { UseShellExecute = true });
+        }
+
+        private void MenuItem3(object Sender, EventArgs e)
+
+        {
+            using (var ps = PowerShell.Create())
+            {
+                ps.AddCommand("Set-ItemProperty").AddParameter("Path", "HKCU:\\Software\\Microsoft\\IME\\15.0\\IMETC").AddParameter("Name", "Left Shift Usage").AddParameter("Value", "0x00000001");
+                var results = ps.Invoke<string>();
+            }
+            this.Close();
+        }
+
 
         private void save_Click(object sender, EventArgs e)
         {
@@ -78,8 +163,12 @@ namespace ApacheGuacamoleIMEswitch
         }
 
 
-        private void button2_Click_1(object sender, EventArgs e)
+
+
+
+        private void switchNotifyLeftShift()
         {
+
             using (var ps = PowerShell.Create())
             {
                 if (!bIsDisable)
@@ -87,20 +176,66 @@ namespace ApacheGuacamoleIMEswitch
                     ps.AddCommand("Set-ItemProperty").AddParameter("Path", "HKCU:\\Software\\Microsoft\\IME\\15.0\\IMETC").AddParameter("Name", "Left Shift Usage").AddParameter("Value", "0x00000002");
                     var results = ps.Invoke<string>();
                     bIsDisable = true;
-                    button2.Text = "按此「啟用本機左邊Shift鍵」";
-                    button2.ForeColor = Color.Blue;
+                    notifyIcon1.ContextMenuStrip.Items[0].Text = "按此「恢復本機左邊Shift鍵」";
+                    notifyIcon1.ContextMenuStrip.Items[0].ForeColor = Color.Blue;
+                    notifyIcon1.ContextMenuStrip.Update();
+
                 }
                 else
                 {
                     ps.AddCommand("Set-ItemProperty").AddParameter("Path", "HKCU:\\Software\\Microsoft\\IME\\15.0\\IMETC").AddParameter("Name", "Left Shift Usage").AddParameter("Value", "0x00000001");
                     var results = ps.Invoke<string>();
                     bIsDisable = false;
-                    button2.Text = "按此「停用本機左邊Shift鍵」";
-                    button2.ForeColor = Color.Red;
+                    notifyIcon1.ContextMenuStrip.Items[0].Text = "按此「暫停本機左邊Shift鍵」";
+                    notifyIcon1.ContextMenuStrip.Items[0].ForeColor = Color.Red;
+                    notifyIcon1.ContextMenuStrip.Update();
+
+                }
+            }
+        }
+
+        private void restoreLeftShift()
+        {
+            string subkey = @"Software\\Microsoft\\IME\\15.0\\IMETC";
+            RegistryKey localKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            if (localKey.OpenSubKey(subkey) != null)
+            {
+                if (localKey.OpenSubKey(subkey).GetValue("Left Shift Usage") != null)
+                {
+                    String value = localKey.OpenSubKey(subkey).GetValue("Left Shift Usage").ToString();
+
+                    if (value.Equals("0x00000001"))
+                    {
+                        bIsDisable = false;
+
+                    }
+                    else
+                    {
+                        bIsDisable = true;
+
+                    }
+
+                }
+
+            }
+            using (var ps = PowerShell.Create())
+            {
+                if (!bIsDisable)
+                {
+                  notifyIcon1.ContextMenuStrip.Items[0].Text = "按此「暫停本機左邊Shift鍵」";
+                  notifyIcon1.ContextMenuStrip.Items[0].ForeColor = Color.Red;
+                    notifyIcon1.ContextMenuStrip.Update();
+                }
+                else
+                {
+                    notifyIcon1.ContextMenuStrip.Items[0].Text = "按此「恢復本機左邊Shift鍵」";
+                    notifyIcon1.ContextMenuStrip.Items[0].ForeColor = Color.Blue;
+                    notifyIcon1.ContextMenuStrip.Update();
                 }
             }
 
         }
+
 
         private void Form1_Closing(object sender, FormClosingEventArgs e)
         {
@@ -122,6 +257,26 @@ namespace ApacheGuacamoleIMEswitch
             strURL = URL.Text.ToString();
             Properties.Settings.Default.URL = URL.Text.ToString();
             Properties.Settings.Default.Save();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+
+            }
+            else//left or middle click
+            {
+                //do something here
+            }
         }
     }
 }
